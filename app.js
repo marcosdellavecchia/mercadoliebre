e2fbb566da0a9d21348a6a31d561da199a6d2a1a
marcosdellavecchia <marcosdvecchia@gmail.com>
2020-10-29 21:15:05 -0300

    Se agregaron pantallas de login y register

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ const path = require("path");
 app.use(express.static("public"));
 
 app.listen(3000, () => {
-  console.log("Server  running on port 3000.");
+  console.log("Server running on port 3000.");
 });
 
-app.get("/", (req, res) => {
+// el metodo 'all' permite que tambien se acepte la solicitud POST (en este caso, para los datos de login y register)
+app.all("/", (req, res) => {
   // path.resolve hace que la ruta sea multiplataforma, evitando problemas con la barra en windows
   res.sendFile(path.resolve(__dirname, "index.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "login.html"));
 });
 
 app.get("*", (req, res) => {
