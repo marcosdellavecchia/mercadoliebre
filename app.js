379a50bf95b87ac273b673d0fc355065e1ee6a65
marcosdellavecchia <marcosdvecchia@gmail.com>
2020-11-13 23:45:33 -0300

    Header and footer fixed

--- app.js
+++ app.js
@@ -6,23 +6,23 @@ const path = require("path");
 app.use(express.static("public"));
 
 app.listen(3000, () => {
-  console.log("Server running on port 3000.");
+    console.log("Server running on port 3000.");
 });
 
 // el metodo 'all' permite que tambien se acepte la solicitud POST (en este caso, para los datos de login y register)
 app.all("/", (req, res) => {
-  // path.resolve hace que la ruta sea multiplataforma, evitando problemas con la barra en windows
-  res.sendFile(path.resolve(__dirname, "index.html"));
+    // path.resolve hace que la ruta sea multiplataforma, evitando problemas con la barra en windows
+    res.sendFile(path.resolve(__dirname, "views/index.html"));
 });
 
 app.get("/register", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "register.html"));
+    res.sendFile(path.resolve(__dirname, "views/register.html"));
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "login.html"));
+    res.sendFile(path.resolve(__dirname, "views/login.html"));
 });
 
 app.get("*", (req, res) => {
-  res.send("Error 404 - No encontramos esta página :/");
+    res.send("Error 404 - No encontramos esta página :/");
 });
