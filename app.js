dff94a93a65b4971e34655eca6de5fe6c11c94af
marcosdellavecchia <marcosdellavecchia@gmail.com>
2020-12-21 19:51:45 -0300

    Se ordeÃnó y comentó el app.js

--- app.js
+++ app.js
@@ -6,15 +6,14 @@ const mainRoutes = require("./routes/mainRoutes");
 const productRoutes = require("./routes/productRoutes");
 const authRoutes = require("./routes/authRoutes");
 
+//Middlewares
 app.use(express.static("public"));
-
 app.use(methodOverride("_method"));
-
 app.use(session({ secret: "Mensaje secreto" }));
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//Motor de vistas
 app.set("view engine", "ejs");
 app.set("views", [
     __dirname + "/views",
@@ -23,14 +22,17 @@ app.set("views", [
     __dirname + "/views/partials",
 ]);
 
+// Levantando el servidor
 app.listen(3000, () => {
     console.log("Server running on port 3000.");
 });
 
+//Rutas
 app.use("/", mainRoutes);
 app.use("/auth", authRoutes);
 app.use("/products", productRoutes);
 
+//404
 app.get("*", (req, res) => {
     res.send("Error 404 - No encontramos esta página :/");
 });
